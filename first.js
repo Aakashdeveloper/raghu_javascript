fb9a94eb8b6470fff54360e5eb34c6d48519a74f
AAKASH HANDA <AAKASH HANDA>
2018-05-15 20:07:55 +0530

    A

---
+++ first.js
@@ -0,0 +1,94 @@
+datatypes
+
+String 
+Number
+Boolean
+
+var a = "raghu"
+var b = "2018raghu"
+var c = 10
+var d = true
+var e = false
+
+=========================================================
+var a = "raghu"
+undefined
+a
+"raghu"
+var b = 10
+undefined
+b
+10
+var c = true
+undefined
+c
+true
+typeof(a)
+"string"
+typeof(b)
+"number"
+typeof(c)
+"boolean"
+==================================================================
+var a = 10
+undefined
+var b = 20
+undefined
+a+b
+30
+a-b
+-10
+a*2
+20
+a*b
+200
+a/b
+0.5
+var a = 10
+undefined
+a%2
+0
+var b = 11
+undefined
+a%2
+0
+b%2
+1
+============================================================
+var a = "raghu"
+undefined
+var b = "   javascript"
+undefined
+a+b
+"raghu   javascript"
+a-b
+NaN
+a*b
+NaN
+a/b
+NaN
+var a = 10
+undefined
+var b = "  raghu"
+undefined
+a+b
+"10  raghu"
+var  c = 10
+undefined
+a+c
+20
+========================================================
+var a = 10
+undefined
+var b = 10
+undefined
+var c = "10"
+undefined
+a+b+c
+"2010"
+c+a+b
+"101010"
+"10"+10+10
+"101010"
+10+"10"+10
+"101010"
